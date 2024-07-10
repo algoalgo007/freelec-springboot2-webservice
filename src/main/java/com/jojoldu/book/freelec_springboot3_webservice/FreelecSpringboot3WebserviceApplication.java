1e7b9424e49713cdc579f4e117520f34e95717c3
YoonKiBum <ykbsuper@naver.com>
2024-07-10 22:21:13 +0900

    modify and bugfix at deploy.yml

--- src/main/java/com/jojoldu/book/freelec_springboot3_webservice/FreelecSpringboot3WebserviceApplication.java
+++ src/main/java/com/jojoldu/book/freelec_springboot3_webservice/FreelecSpringboot3WebserviceApplication.java
@@ -2,6 +2,7 @@ package com.jojoldu.book.freelec_springboot3_webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -9,6 +10,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class FreelecSpringboot3WebserviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(FreelecSpringboot3WebserviceApplication.class, args);
+		SpringApplication application = new SpringApplication(FreelecSpringboot3WebserviceApplication.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(args);
 	}
 }
