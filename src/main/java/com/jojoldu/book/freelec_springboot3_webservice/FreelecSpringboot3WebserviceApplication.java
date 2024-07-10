94ed67610c2609c570866b5deecd63aab5949898
YoonKiBum <ykbsuper@naver.com>
2024-07-10 22:05:39 +0900

    rollback

--- src/main/java/com/jojoldu/book/freelec_springboot3_webservice/FreelecSpringboot3WebserviceApplication.java
+++ src/main/java/com/jojoldu/book/freelec_springboot3_webservice/FreelecSpringboot3WebserviceApplication.java
@@ -2,7 +2,6 @@ package com.jojoldu.book.freelec_springboot3_webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
@@ -10,8 +9,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 public class FreelecSpringboot3WebserviceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication application = new SpringApplication(FreelecSpringboot3WebserviceApplication.class);
-		application.addListeners(new ApplicationPidFileWriter());
-		application.run(args);
+		SpringApplication.run(FreelecSpringboot3WebserviceApplication.class, args);
 	}
 }
